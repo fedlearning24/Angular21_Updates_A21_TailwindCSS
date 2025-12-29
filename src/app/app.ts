ae3a14eb7238a60dd6586bb115deecf7a4438802
sushma <sushhonna08@gmail.com>
2025-12-29 12:57:38 +0530

    push all code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { email, Field, form, maxLength, minLength, required } from '@angular/forms/signals';
+
+interface ILoginData {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Field, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('A21_TailwindCSS');
+  isActive = signal<boolean>(false);
+
+  isVisible = signal<boolean>(true);
+
+  headingStyles = signal({
+    'border':'1px solid black',
+    'font-weight':'bold'
+  })
+
+   loginModel = signal<ILoginData>({
+    email: '',
+    password: ''
+  })
+
+  loginForm = form(this.loginModel, (schemaPath) => {
+    required(schemaPath.email, { message: 'Email is required.' });
+    email(schemaPath.email, { message: 'Enter a valid email address.' });
+    required(schemaPath.password, { message: 'Password is required.' });
+    minLength(schemaPath.password, 5, { message: 'Password must be at least 5 charactors.' });
+    maxLength(schemaPath.password, 10, { message: 'Password is too long.' });
+  });
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    console.log(this.loginModel());
+  }
+
 }
